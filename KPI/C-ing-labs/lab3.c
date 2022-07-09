ec2aaab39db922c7a47e221969b527b2b92bfefb
BolgarinV <75188605+ONIQdevv@users.noreply.github.com>
2022-07-09 13:33:24 +0200

    Correct.

--- KPI/C-ing-labs/lab3.c
+++ KPI/C-ing-labs/lab3.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 
 void printName (void);
-void printGrade(double ave_grade);
 double averageGrade(unsigned int number_of_subjects);
-
-int main() {
+void printGrade( double ave_grade );
+int main()
+    {
 
     unsigned int subjects;
     double mean;
@@ -16,49 +16,60 @@ int main() {
     printf("\nOr enter ZERO to exit this program");
     printf("\n\nsubjects= ");
     scanf("%u", &subjects);
-    if(subjects == 0)
-        exit(0);
-    mean = averageGrade(subjects);
-    printGrade(mean);
+    if(subjects == 0 )
+    exit(0);
+    mean = averageGrade( subjects );
+    printGrade( mean );
     return 0;
 }
+
 void printName (void) {
     char* name = malloc(sizeof (char*)*100);
     printf("Enter your surname and name: ");
     scanf("%s", name);
 }
 
-double averageGrade(unsigned int number_of_subjects) {
 
+double averageGrade(unsigned int number_of_subjects)
+{
     unsigned int grade, sum;
     double mean_value;
-
     system("cls");
     printf("Enter grades for each subject. Grade must be between 60...100.");
     printf("\nTotal number of grades equals to %u\n", number_of_subjects);
     sum = 0;
-    int i = 1;
+    unsigned int i = 1;
     while(i <= number_of_subjects) {
-        while(grade < 60 || grade > 100) {
-            printf("\ngrade #%u: ", i);
+        do{
+            printf("\ngrade %u: ", i);
             scanf("%u", &grade);
-        }
-        sum = sum + grade;
+        } while( grade < 60 || grade > 100 );
+
+        sum += grade;
         i++;
+
     }
     mean_value = (double)sum / (double)number_of_subjects;
 
-    if (mean_value >= 60 && mean_value <= 64) printf("Average grade: %.3lf - Sufficient", mean_value);
-    if (mean_value >= 65 && mean_value <= 74) printf("Average grade: %.3lf - Satisfactory", mean_value);
-    if (mean_value >= 75 && mean_value <= 84) printf("Average grade: %.3lf - Good", mean_value);
-    if (mean_value >= 85 && mean_value <= 94) printf("Average grade: %.3lf - Very good", mean_value);
-    if (mean_value >= 94 && mean_value <= 100) printf("Average grade: %.3lf - Excellent", mean_value);
+//     if (mean_value >= 60 && mean_value <= 64) printf("Average grade: %.3lf - Sufficient", mean_value);
+//     if (mean_value >= 65 && mean_value <= 74) printf("Average grade: %.3lf - Satisfactory", mean_value);
+//     if (mean_value >= 75 && mean_value <= 84) printf("Average grade: %.3lf - Good", mean_value);
+//     if (mean_value >= 85 && mean_value <= 94) printf("Average grade: %.3lf - Very good", mean_value);
+//     if (mean_value >= 94 && mean_value <= 100) printf("Average grade: %.3lf - Excellent", mean_value);
 
     return mean_value;
+
 }
 
 void printGrade( double ave_grade ) {
-    system("cls");
-    printf("\n");
-    printf("Average grade: %.3lf\n", ave_grade);
+ system("cls");
+ printf("\n");
+ printf("Average grade: %.1lf\n", ave_grade);
+
+ if (ave_grade >= 60 && ave_grade <= 64) printf("Average grade: %.3lf - Sufficient", ave_grade);
+ if (ave_grade >= 65 && ave_grade <= 74) printf("Average grade: %.3lf - Satisfactory", ave_grade);
+ if (ave_grade >= 75 && ave_grade <= 84) printf("Average grade: %.3lf - Good", ave_grade);
+ if (ave_grade >= 85 && ave_grade <= 94) printf("Average grade: %.3lf - Very good", ave_grade);
+ if (ave_grade >= 94 && ave_grade <= 100) printf("Average grade: %.3lf - Excellent", ave_grade);
+
 }
--- KPI/C-ing-labs/lab4.c
+++ KPI/C-ing-labs/lab4.c
@@ -1,107 +1,123 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Initialization
-void fill_int_array( unsigned int * uiptr, unsigned int size_of_array );
-void fill_float_array( float * fptr, unsigned int size_of_array );
-void print_int_array( const unsigned int * uiptr, unsigned int size_of_array );
-void print_float_array( const float * fptr, unsigned int size_of_array);
+void fill_int_array(unsigned int* uiptr, unsigned int size_of_array);
+void fill_float_array(float* fptr, unsigned int size_of_array);
+void print_int_array(const unsigned int* uiptr, unsigned int size_of_array);
+void print_float_array(const float* fptr, unsigned int size_of_array);
+unsigned int sum(const unsigned int* uiptr, unsigned int size_of_array);
+double product(const float* fptr, unsigned int size_of_array);
+double mean_value(const unsigned int* A, unsigned int Size);
+unsigned int find_min(const unsigned int* A, unsigned int Size);
+float find_max(const float* B, unsigned int Size);
 
-unsigned int sum ( const unsigned int * uiptr, unsigned int size_of_array );
-double product( const float * fptr, unsigned int size_of_array );
-
-// Definition of main func
 int main() {
-    unsigned int * A;
-    float * B;
+    unsigned int* A;
+    float* B;
     unsigned int Size; // Змінна для збережння розміру динамічного масиву
     unsigned int amount; // змінна для збереження суми елементів масиву А
     double mult; // змінна для збереження добутку елементів масиву В
     printf("Enter size of array: ");
     scanf("%u", &Size);
-    A = (unsigned int *) calloc (Size, sizeof(unsigned int) );
-    B = (float *) malloc (Size * sizeof(float) );
-    if( A == NULL || B == NULL ){
+    A = (unsigned int*) calloc(Size, sizeof(unsigned int));
+    B = (float*) malloc(Size* sizeof(float));
+    if (A == NULL || B == NULL) {
         printf("Memory has not been allocated");
         exit(0);
-
     }
-    fill_int_array( A, Size );
-    fill_float_array( B, Size );
-    print_int_array( A, Size );
-    print_float_array( B, Size );
-    amount = sum( A, Size );
-    mult = product( B, Size );
+
+    fill_int_array(A, Size);
+    fill_float_array(B, Size);
+    print_int_array(A, Size);
+    print_float_array(B, Size);
+    amount = sum(A, Size);
+    mult = product(B, Size);
+    double average_value = mean_value(A, Size);
+    unsigned int min_value = find_min(A, Size);
+    float max_value = find_max(B, Size);
     printf("\n\nSum = %u", amount);
     printf("\nProduct = %.3lf", mult);
+    printf("\nAvverage = %.3lf", average_value);
+    printf("\nMIN = %d", min_value);
+    printf("\nMAX = %.3lf", max_value);
+    printf("\n");
     free(A);
     free(B);
     return 0;
-
 }
 
-//----------- Other functions definition ----------------
-//-------------------------------------------------------
-void fill_int_array( unsigned int * A, unsigned int Size ) {
+void fill_int_array(unsigned int* A, unsigned int Size) {
     unsigned int j;
-    unsigned int temp;
     printf("\n\nEnter values of UNSIGNED INT elements of array.\n");
-    for( j = 0; j <= Size-1; j++ ){
-        printf("A[%u]= ", j);
-        scanf("%u", &temp);
-        A[j] = temp;
-
+    for (j = 0; j <= Size - 1; j++){
+        A[j] = Size - (j + 1);
     }
-
 }
-//---------------------------------------------------------
-void fill_float_array( float * B, unsigned int Size ) {
+
+void fill_float_array(float* B, unsigned int Size) {
     unsigned int j;
-    float temp;
     printf("\n\nEnter values of FLOAT elements of array.\n");
-    for( j = 0; j <= Size-1; j++ ){
-        printf("B[%u]= ", j);
-        scanf("%f", &temp);
-        B[j] = temp;
-
+    for (j = 0; j <= Size-1; j++) {
+        B[j] = 5.0 * j + 5.0;
     }
-
 }
-//-------------------------------------------------------------
-void print_int_array( const unsigned int * A, unsigned int Size ) {
+
+void print_int_array(const unsigned int* A, unsigned int Size) {
     unsigned int j;
     printf("\n\nArray of UNSIGNED INT values:\n");
-    for( j = 0; j < Size; j++ ) {
-        printf("%5d", A[j] );
-
-    }
-
+    for (j = 0; j < Size; j++)
+        printf("%5d", A[j]);
 }
-//-------------------------------------------------------------
 
-void print_float_array( const float * B, unsigned int Size ) {
+void print_float_array(const float* B, unsigned int Size) {
     unsigned int j;
     printf("\n\nArray of FLOAT values:\n");
-    for( j = 0; j < Size; j++ ) {
-        printf("%7.2f", B[j] );
-    }
+    for (j = 0; j < Size; j++)
+        printf("%7.2f", B[j]);
 }
-//---------------------------------------------------------------
-unsigned int sum (const unsigned int * A, unsigned int Size ) {
+
+unsigned int sum(const unsigned int* A, unsigned int Size) {
     unsigned int j, S;
-    S = 0; // змінна, яка використовується для обрахунку значення суми елементів масиву
-    for( j = 0; j < Size; j++ )
+    S = 0;
+    
+    for (j = 0; j < Size; j++)
         S += A[j];
     return S;
 }
-//---------------------------------------------------------------
-double product(const float * B, unsigned int Size ) {
+
+double product(const float* B, unsigned int Size) {
     unsigned int j;
     double M;
-    M = 1.0; //змінна, яка використовується для обрахунку значення добутку елементів масиву
-    for( j = 0; j < Size; j++ ) {
+    M = 1.0;
+
+    for (j = 0; j < Size; j++)
         M *= B[j];
+    return M;
+}
+
+double mean_value(const unsigned int* A, unsigned int Size) {
+    unsigned int S = 0;
+    for (unsigned int j = 0; j < Size; j++)
+        S += A[j];
+
+    return (float) S / Size;
+}
+
+unsigned int find_min(const unsigned int* A, unsigned int Size) {
+    unsigned int M = A[0];
+
+    for (unsigned int j = 0; j < Size; j++) {
+        if (A[j] < M) M = A[j];
     }
     return M;
 }
-//---------------------------------------------------------------
+
+float find_max(const float* B, unsigned int Size) {
+    float M = B[0];
+
+    for (unsigned int j = 0; j < Size; j++) {
+        if (B[j] > M) M = B[j];
+    }
+
+    return M;
+}
